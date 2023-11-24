1a1461ee7f2dfdfea244d278dc30300a98fa333d
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-11-25 03:03:16 +0600

    done

--- Ans1.sh
+++ Ans1.sh
@@ -1,18 +1,31 @@
-#author Shakil Ahamed Riaz (221-35-995)
+# Author Shakil Ahamed Riaz (221-35-995)
 #!/bin/bash
-# Enter a positive integer as nth of sum
-echo "Enter a positive integer limit:"
-read limit
+# Function for calculating the average of an array
+calculate_average() {
+    local sum=0
+    local count=0
 
-# Initialize sum to 0
-sum=0
+    for num in "${numbers[@]}"; do
+        sum=$(echo "$sum + $num" | bc)
+        count=$((count + 1))
+    done
 
-# Use a for loop to calculate the sum
-for (( i=1; i<=$limit; i++ ))
-do
-    sum=$((sum + i))
-done
+    average=$(echo "scale=$precision; $sum / $count" | bc)
+    echo "Average with precision $precision: $average"
+}
 
-# Display the result
-echo "The sum of numbers from 1 to $limit is: $sum"
+# Prompt the user to enter the floating-point numbers
+echo "Enter the floating-point numbers separated by spaces:"
+read -r input
+# Replace spaces with line breaks and store in the array
+numbers=($(echo "$input" | tr ' ' '\n'))
+
+# Prompt the user to enter the precision for the average
+echo "Enter the precision for the average (number of decimal places):"
+read -r precision
+
+# Call the function to calculate and display the average
+calculate_average
+
+
 
